082b9559dea2cac14e1b3a1e2170448a411bb382
⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯ <hi2879639@gmail.com>
2025-08-26 17:51:18 +0300

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "owner",
-    description: "Onyesha namba ya owner",
+    description: "show owner phone number",
     async execute(sock, msg, args, settings) {
         const from = msg.key.remoteJid;
         await sock.sendMessage(from, { text: `👑 Owner: ${settings.ownerNumber}` });
