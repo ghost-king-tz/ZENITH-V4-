b0515769f10d9f8536a42278d258317c1fa64ff0
⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯ <hi2879639@gmail.com>
2025-09-15 12:32:04 +0300

    Delete commands directory

--- commands/owner.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "owner",
-    description: "show owner phone number",
-    async execute(sock, msg, args, settings) {
-        const from = msg.key.remoteJid;
-        await sock.sendMessage(from, { text: `👑 Owner: ${settings.ownerNumber}` });
-    }
-};
