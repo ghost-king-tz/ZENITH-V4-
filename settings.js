5aa85bd6bf880bd53021f121af183779a90b6867
⎯𓆩〬𝐁𝐋𝐀𝐂𝐊 𝔅𝔏𝔄𝔃𝔈𓍢𓆪 𓆩𓂍〬𝔅𝔯𝔬𝔨𝔢𝔫 𝕊𝕠𝕦𝕝𓆪𓍯 𓆩〬𝐃𝐀𝐑𝐊 𝔇𝔞𝔴𝔫𓂍𓆪⎯ <hi2879639@gmail.com>
2025-09-15 12:34:53 +0300

    Delete settings.js

--- settings.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    ownerNumber: process.env.OWNER_NUMBER || "255719632816",
-    botName: process.env.BOT_NAME || "Zenith V4",
-    prefix: process.env.PREFIX || "!",
-    sessionPath: process.env.SESSION_PATH || "./session"
-};
